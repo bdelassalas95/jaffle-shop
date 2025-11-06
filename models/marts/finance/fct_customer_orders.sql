897b44aba0988e6cf759dc43aa668171d7a642b0
Bryan De Las Salas <bdelassalas@gmail.com>
2025-11-06 05:09:17 +0000

    finished refactoring of fct_customer_orders.sql, broke parts of logic into int_orders.sql

--- models/marts/finance/fct_customer_orders.sql
+++ models/marts/finance/fct_customer_orders.sql
@@ -2,102 +2,30 @@ with
 
 orders as (
 
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+    select * from {{ ref('int_orders') }}
 
 ),
 
-customers as (
-
-    select * from {{ ref('stg_jaffle_shop__customers') }}
-
-),
-
-payments as (
+final as (
 
     select
 
+        customer_id,
         order_id,
-        max(payment_date) as payment_finalized_date,
-        sum(payment_amount) as total_amount_paid
-    
-    from {{ ref('stg_stripe__payment') }}
-
-    where payment_status <> 'fail'
-
-    group by 1
-
-),
-
-paid_orders as (
-
-    select
-    
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date as order_placed_at,
-        orders.order_status,
-        payments.total_amount_paid,
-        payments.payment_finalized_date,
-        customers.first_name as customer_first_name,
-        customers.last_name as customer_last_name
+        order_placed_at,
+        order_status,
+        total_amount_paid,
+        payment_finalized_date,
+        customer_first_name,
+        customer_last_name,
+        transaction_seq,
+        customer_sales_seq,
+        case when customer_first_order_date = order_placed_at then 'new' else 'return' end as nvsr,
+        customer_lifetime_value,
+        first_order_date as fdos
 
     from orders
 
-    left join payments using (order_id)
-
-    left join customers using (customer_id)
-
-),
-
-customer_orders as (
-    
-    select
-
-        customers.customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(orders.order_id) as number_of_orders
-
-    from customers
-
-    left join orders using (customer_id)
-
-    group by 1
-    
 )
 
-    select
-
-        p.*,
-
-        row_number() over (order by p.order_id) as transaction_seq,
-
-        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-
-        case when c.first_order_date = p.order_placed_at then 'new' else 'return' end as nvsr,
-
-        x.clv_bad as customer_lifetime_value,
-
-        c.first_order_date as fdos
-
-    from paid_orders p
-
-    left join customer_orders as c using (customer_id)
-
-    left outer join (
-    
-        select
-            p.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-
-        from paid_orders p
-        
-        left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-        
-        group by 1
-        
-        order by p.order_id
-
-    ) x on x.order_id = p.order_id
-    
-    order by order_id
+select * from final
