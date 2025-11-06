d6be8a57fab5e823b1bcdcaa75d49ad69fdd5216
Bryan De Las Salas <bdelassalas@gmail.com>
2025-11-06 05:44:17 +0000

    minor changes to refactoring of fct_customers_orders.sql

--- models/intermediate/int_orders.sql
+++ models/intermediate/int_orders.sql
@@ -12,9 +12,9 @@ orders as (
 
 ),
 
-customers as (
+base_payments as (
 
-    select * from {{ ref('stg_jaffle_shop__customers') }}
+    select * from {{ ref('stg_stripe__payment') }}
 
 ),
 
@@ -24,10 +24,11 @@ payments as (
 
         order_id,
         sum(payment_amount) as total_amount_paid,
-        max(payments.payment_date) as payment_finalized_date
+        max(payment_date) as payment_finalized_date
     
-    from {{ ref('stg_stripe__payment') }}
+    from base_payments
     where payment_status <> 'fail'
+    group by 1
 
 ),
 
@@ -41,8 +42,6 @@ customer_paid_orders as (
         orders.order_status,
         payments.total_amount_paid,
         payments.payment_finalized_date,
-        customers.first_name as customer_first_name,
-        customers.last_name as customer_last_name,
 
         row_number() over (order by orders.order_id) as transaction_seq,
         row_number() over (partition by orders.customer_id order by orders.order_id) as customer_sales_seq,
@@ -50,14 +49,11 @@ customer_paid_orders as (
         --Customer aggregations--
         min(order_date)                 over(partition by orders.customer_id)   as customer_first_order_date,
         max(order_date)                 over(partition by orders.customer_id)   as customer_most_recent_order_date,
-        count(orders.order_id)          over(partition by orders.customer_id)   as customer_number_of_orders,
         sum(payments.total_amount_paid) over(partition by orders.customer_id)   as customer_lifetime_value,
 
     from orders
 
     left join payments using (order_id)
-
-    left join customers using (customer_id)
 
 )
 
--- models/marts/finance/fct_customer_orders.sql
+++ models/marts/finance/fct_customer_orders.sql
@@ -6,6 +6,12 @@ orders as (
 
 ),
 
+customers as (
+
+    select * from {{ ref('stg_jaffle_shop__customers') }}
+
+),
+
 final as (
 
     select
@@ -16,15 +22,17 @@ final as (
         order_status,
         total_amount_paid,
         payment_finalized_date,
-        customer_first_name,
-        customer_last_name,
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name,
         transaction_seq,
         customer_sales_seq,
-        case when customer_first_order_date = order_placed_at then 'new' else 'return' end as nvsr,
+        case when customer_first_order_date = customer_most_recent_order_date then 'new' else 'return' end as nvsr,
         customer_lifetime_value,
-        first_order_date as fdos
+        customer_first_order_date as fdos
 
     from orders
+
+    left join customers using (customer_id)
 
 )
 
