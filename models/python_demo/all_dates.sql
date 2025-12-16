96cf72a54ea5226836128880f050266e80b09b9c
Bryan De Las Salas <bdelassalas@gmail.com>
2025-12-16 01:57:32 +0000

    created python model is_holiday_2025.py

--- models/python_demo/all_dates.sql
+++ models/python_demo/all_dates.sql
@@ -5,7 +5,7 @@
 
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="cast('2020-01-01' as date)",
-    end_date="cast('2021-01-01' as date)"
+    start_date="cast('2025-01-01' as date)",
+    end_date="cast('2026-01-01' as date)"
    )
 }}
